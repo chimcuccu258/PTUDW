1cec5090dfcfc86477f8065096fcb1715e50a2fb
Minh Nga <vuminhnga25@gmail.com>
2023-11-26 09:42:24 +0700

    update 26/11

---
+++ QLVinpearl_63130803/Areas/Admin/Controllers/PermissionError_63130803Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLVinpearl_63130803.Areas.Admin.Controllers
+{
+    public class PermissionError_63130803Controller : Controller
+    {
+        // GET: Admin/PermissionError_63130803
+        public ActionResult NotAllowPermission()
+        {
+            return View();
+        }
+    }
+}
